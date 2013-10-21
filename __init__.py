5af89723796e27c50a0b6b37c1850cdae398cf76
Arnaud Réveillère <arnaud.reveillere@centraliens.net>
2013-10-21 23:47:30 +0200

    Create __init__.py
    __init__ file

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# SAMBA is a computer program that solves the problem of leakage between connected aquifers using a semi-analytic method
+# It is running on Windows 7 32 bits with python-2.7.1 + numpy-1.6.0 + scipy-0.9.0
+# It has been developed under BRGM research project CO2 storage risks management
+# 
+# Copyright (C) 2012 Arnaud REVEILLERE
+# Contact: a.reveillere at brgm.fr / arnaud.reveillere @ centraliens.net
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+from BrineProperties_1_0 import *
+from BrinePropertiesLinearized_1_0 import *
+from LeakageModels_1_0 import *
+from TOUGH2PreAndPostProcessing_1_0 import *
